d4b5cb06b67858b9da03f18f3481af15fe304834
azessar <akzessar@gmail.com>
2020-09-21 20:44:29 -0500

    oops this is for auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::SessionsController < ApplicationController
         )
 
         if @user
-            login(@user)
+            login!(@user)
             render "api/users/show"
         else
             render json: ["This email and password do not match."], status: 401
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,16 +2,27 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user, :logged_in?
 
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
     def current_user
         return nil unless session[:session_token]
         @current_user ||= User.find_by(session_token: session[:session_token])
     end
 
+   def logout!
+        current_user = reset_session_token!
+        @current_user = nil
+        session[:session_token] = nil
+    end
+
     def ensure_logged_in
-        if current_user
-        else
-            redirect_to new_session_url 
-        end
+        redirect_to new_session_url unless logged_in?
+    end
+
+    def logged_in?
+        !!current_user
     end
 
 end
