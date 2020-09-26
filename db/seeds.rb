674f63928ddee39bd82424ec610a56d9c17078b7
azessar <akzessar@gmail.com>
2020-09-26 18:35:51 -0500

    adding seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-
+User.destroy_all
 user1 = User.create([{first_name: 'Andrew'}, {last_name: 'Zessar'}, {email: 'blah@gmail.com'}, {primary_dining_location: 'Chicago'}, {password: 'password'}])
 user2 = User.create([{first_name: 'bob'}, {last_name: 'bob'}, {email: 'bob'}, {primary_dining_location: 'Chicago'}, {password: 'password'}])
+user3 = User.create([{first_name: 'Gordon'}, {last_name: 'Ramsay'}, {email: 'gramsay@gmail.com'}, {primary_dining_location: 'London'}, {password: 'rubbish'}])
