074242fa0d49294b9c1f41046bf1f04e5b281a24
azessar <akzessar@gmail.com>
2020-11-07 17:34:47 -0600

    working on res page

--- frontend/components/reservations/reservation_form.jsx
+++ frontend/components/reservations/reservation_form.jsx
@@ -21,11 +21,14 @@ class ReservationForm extends React.Component {
         this.props.fetchRestaurant(this.props.match.params.restaurantId);
     }
 
-
+    // let monthArray = {
+    //     1: 1
+    // }
 
     render() {
         const restaurant = this.props.restaurant;
         let formDate = this.props.location.state ? this.props.location.state.resDate : this.state.resDate;
+        // console.log(typeof formDate.slice(0,4))
         let formTime = this.props.location.state ? this.props.location.state.resTime : "7:00 PM"
         let formParty = this.props.location.state ? this.props.location.state.partySize : 2;
         if (!restaurant) {
@@ -38,7 +41,26 @@ class ReservationForm extends React.Component {
                     <div>United States</div>
                     <div>{restaurant.city} / {restaurant.state}</div>
                 </div>
-                <div>hello {restaurant.name} blah blah {formParty} blah {formTime} blah {formDate} </div>
+                <div className="res-form-body">
+                    <div className="almost-done">
+                        You're almost done!
+                    </div>
+                    <div className="res-card">
+                        <img className="res-pic" src={restaurant.main_photo} />
+                        <div className="res-info">
+                            <div className="res-name-city">
+                                {restaurant.name} - {restaurant.city}
+                            </div>
+                            <div className="res-date-time-peeps">
+                                <div className="res-form-date">{formDate}</div>
+                                <div className="res-form-time">{formTime}</div>
+                                <div className="res-form-peeps">{formParty}</div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                </div>
             </div>
         )
     }
