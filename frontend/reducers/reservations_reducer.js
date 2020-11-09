4947f1ed6f09d967dab40ee1b99af294f86f1a30
azessar <akzessar@gmail.com>
2020-11-09 08:48:49 -0600

    got reservations to work

--- frontend/reducers/reservations_reducer.js
+++ frontend/reducers/reservations_reducer.js
@@ -4,12 +4,12 @@ const reservationsReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_ALL_RESERVATIONS:
-            return action.reservations;
+            return Object.assign({}, action.reservations);
         case RECEIVE_RESERVATION:
-            return merge({}, state, { [action.reservation.id]: action.reservation });
+            return Object.assign({}, state, { [action.reservation.id]: action.reservation });
         case DELETE_RESERVATION:
-            let newState = merge({}, state);
-            delete newState[action.reservationId];
+            let newState = Object.assign({}, state);
+            delete newState[action.resId];
             return newState;
         default:
             return state;
